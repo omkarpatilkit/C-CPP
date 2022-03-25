6fab782a71890d12430acba448bd17e254fd14a9
Omkar <omkarpatil3462@gmail.com>
2022-03-25 19:22:59 +0530

    Create divide.c

---
+++ Basic/divide.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+double divide( int a , int b){
+if (a < b){
+    return -1 ;
+}
+else{
+    return a/b;
+}
+
+}
+
+int main (){
+
+int iVal1, iVal2;
+double iRes;
+
+
+printf("Please enter first value: \n" );
+scanf("%d", &iVal1);
+
+printf("Please enter second value: \n" );
+scanf("%d", &iVal2);
+
+iRes = divide(iVal1, iVal2);
+if (iRes < 0)
+{
+    printf("Please enter valid number: \n" );
+
+}
+
+else{
+    printf("the division is %lf", iRes);
+}
+    return 0;
+}
